efc19c65212cc22a38bd38f0f7ec68fe229faee2
bd2187 <brandon.dionisio.26@gmail.com>
2019-06-10 19:18:38 -0700

    Write logic to update the game board. Write logic to calculate winner of the game

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // Store the gameboard as an array inside of a Gameboard object
 
 // Use modular pattern for game_board
-const game_board = () => {
+const game_board = (() => {
     // 0 1 2
     // 3 4 5
     // 6 7 8
@@ -51,26 +51,51 @@ const game_board = () => {
     var turn = "";
     var calculate_winner = () => {
         for (let i = 0; i < positions.length; i++) {
-            for (let j = 0; j < positions[i].length; j++) {}
+            let row = positions[i];
+            let winner = false;
+            if (
+                row[0].player !== "" &&
+                row[0].player === row[1].player &&
+                row[1].player === row[2].player
+            ) {
+                console.log(row);
+                winner = true;
+            }
+
+            if (winner) {
+                console.log("winner");
+                return;
+            }
         }
     };
 
     var update_game_board = (player, position) => {
-        positions.forEach(function(position_object) {
-            if (position_object.position === position) {
-                position.player = player;
-            }
+        positions = positions.map(function(row) {
+            return row.map(function(position_object) {
+                if (
+                    position_object.player === "" &&
+                    position_object.position === position
+                ) {
+                    position_object.player = player;
+                }
+
+                return position_object;
+            });
         });
 
         calculate_winner();
     };
     return { update_game_board, turn };
-};
+})();
 
 // Use factory function for player
 const Player = (player, position) => {
     return {};
 };
 
-const jon = Player("jon");
-const jane = Player("jane");
+// const jon = Player("jon");
+// const jane = Player("jane");
+
+// game_board.update_game_board("brandon", 0);
+// game_board.update_game_board("brandon", 1);
+// game_board.update_game_board("brandon", 2);
